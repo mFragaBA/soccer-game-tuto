79cbf394564d95b876b9a0559b816b01cc556412
Martin Fraga <fragamartine@gmail.com>
2025-06-11 00:12:41 -0300

    add basic movement and control schemes

---
+++ scenes/Characters/player.gd
@@ -0,0 +1,46 @@
+class_name Player
+extends CharacterBody2D
+
+enum ControlScheme {CPU, P1, P2}
+
+@export var control_scheme : ControlScheme
+@export var speed : float
+
+@onready var animation_player : AnimationPlayer = %AnimationPlayer
+@onready var player_sprite : Sprite2D = %PlayerSprite
+
+var heading : Vector2 = Vector2.RIGHT
+
+func _process(delta: float) -> void:
+	if control_scheme == ControlScheme.CPU:
+		pass
+	else:
+		handle_human_movement()
+
+func handle_human_movement() -> void:
+	var direction := KeyUtils.get_input_vector(control_scheme)
+	velocity = direction * speed
+	
+	# TODO: should it be moved to physics process?
+	move_and_slide()
+	set_movement_animation()
+	set_heading()
+	flip_sprite()
+	
+func set_movement_animation() -> void:
+	if velocity.length() > 0:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
+
+func set_heading() -> void:
+	if velocity.x < 0:
+		heading = Vector2.LEFT
+	elif velocity.x > 0:
+		heading = Vector2.RIGHT
+		
+func flip_sprite() -> void:
+	if heading == Vector2.RIGHT:
+		player_sprite.flip_h = false
+	elif heading == Vector2.LEFT:
+		player_sprite.flip_h = true
