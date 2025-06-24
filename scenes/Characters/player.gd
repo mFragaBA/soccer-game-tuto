747932eb96000129222ee7d36158d69d59892986
Martin Fraga <fragamartine@gmail.com>
2025-06-24 00:02:23 -0300

    add walk animation

--- scenes/Characters/player.gd
+++ scenes/Characters/player.gd
@@ -11,6 +11,7 @@ const CONTROL_SCHEME_MAP : Dictionary = {
 const COUNTRIES := ["DEFAULT", "FRANCE", "ARGENTINA", "BRAZIL", "ENGLAND", "GERMANY", "ITALY", "SPAIN", "USA", "BOCA", "INDEPENDIENTE", "RIVER", "RACING", "SAN LORENZO"]
 
 const GRAVITY := 8.0
+const WALK_ANIM_THRESHOLD := 0.6
 
 enum ControlScheme {CPU, P1, P2}
 enum State {MOVING, TACKLING, RECOVERING, PREPPING_SHOT, SHOOTING, PASSING, HEADER, VOLLEY_KICK, BICYCLE_KICK, CHEST_CONTROL}
@@ -93,10 +94,14 @@ func setup_ai() -> void:
 	add_child(ai_behavior)
 	
 func set_movement_animation() -> void:
-	if velocity.length() > 0:
-		animation_player.play("run")
-	else:
+	var vel_length := velocity.length()
+	if vel_length < 1:
 		animation_player.play("idle")
+	elif vel_length < speed * WALK_ANIM_THRESHOLD:
+		animation_player.play("walk")
+	else:
+		animation_player.play("run")
+
 
 func process_gravity(delta: float) -> void:
 	if height > 0:
